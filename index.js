fc51126ed53b775e6b34dfedd9ac365efe741323
cvanstaen <clement.van.staen@zalando.de>
2020-09-02 16:01:01 +0200

    First push

---
+++ index.js
@@ -0,0 +1,4 @@
+const User = require("./mockData/user");
+
+const user1 = new User("Clément", " van Staen", 34, "Berlin");
+user1.greeting();
---
+++ references/fs_demo.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+
+/* SHIFT + OPT + A to comment */
+
+// Create folder
+fs.mkdir(path.join(__dirname, "/test"), {}, err => {
+    if (err) throw err;
+    console.log('Folder created!')
+});
+
+// Create and write to files
+fs.writeFile(path.join(__dirname, "/test", 'hello.txt'), 'Hello world!', err => {
+    if (err) throw err;
+    console.log('File created!')
+    // File Append
+    fs.appendFile(path.join(__dirname, "/test", 'hello.txt'), ' I love node.js', err => {
+        if (err) throw err;
+        console.log('File appended!')
+    });
+});
+
+// Read file
+fs.readFile(path.join(__dirname, "/test", 'hello.txt'), 'utf-8', (err, data) => {
+    if (err) throw err;
+    console.log(data)
+});
+
+// Rename
+fs.rename(path.join(__dirname, "/test", 'hello.txt'), path.join(__dirname, "/test", 'helloworld.txt'), err => {
+    if (err) throw err;
+    console.log('File renamed!')
+});
