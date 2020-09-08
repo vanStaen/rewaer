6df24266ff773057bc287d2e6464c98ee3765e3c
cvanstaen <clement.van.staen@zalando.de>
2020-09-08 18:45:06 +0200

    Prepare index for new branch

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+// NODE ONLY
+/*
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 
-    /*  if (req.url === '/') {
-            fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-                if (err) throw err;
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(content);
-            })
-        }
-        if (req.url === '/about') {
-            fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-                if (err) throw err;
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(content);
-            })
-        } */
+    //    if (req.url === '/') {
+    //        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+    //            if (err) throw err;
+    //            res.writeHead(200, { 'Content-Type': 'text/html' });
+    //            res.end(content);
+    //        })
+    //    }
+    //    if (req.url === '/about') {
+    //        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+    //            if (err) throw err;
+    //            res.writeHead(200, { 'Content-Type': 'text/html' });
+    //            res.end(content);
+    //        })
+    //    }
 
     // Build file path
     let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
@@ -86,4 +88,4 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Serveur running on Port ${PORT}`));
+server.listen(PORT, () => console.log(`Serveur running on Port ${PORT}`)); */
