3316882bf77a5d457bc2e269c17aeb8a3ddb8101
cvanstaen <clement.van.staen@zalando.de>
2020-09-08 23:05:42 +0200

    Save log in txt file

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const path = require('path');
 const test = require('./test');
 const uuid = require('uuid');
 const moment = require('moment');
+const fs = require("fs");
 const PORT = process.env.PORT || 5000;
-
-
-const logger = (req, res, next) => {
-    const url = req.protocol + '://' + req.get('host') + req.originalUrl;
-    console.log(`${uuid.v4()} - ${moment().format("DD/MM/YYYY, H:mm:ss")} > GET: ${url} `);
-    next();
-};
 
 
 // Init Express
 const app = express();
+
+// Logging function
+const logger = (req, res, next) => {
+    const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    const logLine = uuid.v4() + ' - ' + moment().format("DD/MM/YYYY, H:mm:ss") + ' > GET: ' + url + '\r\n';
+    fs.appendFile(path.join(__dirname, 'log.txt'), logLine, 'utf8', err => {
+        if (err) throw err;
+    });
+    next();
+};
 
 // Initialise Middleware function
 app.use(logger);
