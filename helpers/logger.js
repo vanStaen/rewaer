cccd14cfe69f2335d744525482e2303c06ebb300
cvanstaen <clement.van.staen@zalando.de>
2020-09-09 11:41:13 +0200

    Logger format change

--- helpers/logger.js
+++ helpers/logger.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const logger = (req, res, next) => {
 
     const url = req.protocol + '://' + req.get('host') + req.originalUrl;
-    const logLine = uuid.v4() + ' - ' + moment().format("DD/MM/YYYY, H:mm:ss") + ' > GET: ' + url + '\r\n';
+    const logLine = ' > GET: ' + url + ' (id: ' + uuid.v4() + ') - ' + moment().format("DD/MM/YYYY, H:mm:ss") + '\r\n';
 
     fs.appendFile(path.join(__dirname, '../', 'log.txt'), logLine, 'utf8', err => {
         if (err) throw err;
