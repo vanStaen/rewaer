23b80315af36fbd7ba003c11c15c21eca2c966d3
cvanstaen <clement.van.staen@zalando.de>
2020-09-09 13:47:39 +0200

    mock data users

---
+++ helpers/mockData/mockDataUsers.js
@@ -0,0 +1,22 @@
+/* rewaer_users */
+const users = [
+    {
+        id: 0,
+        userName: 'Clément',
+        googleId: 10002030432532450,
+        picId: 0,
+        dateCreated: '01.10.2020',
+        statusActive: true
+    },
+
+    {
+        id: 1,
+        userName: 'Theresa',
+        googleId: 12342549080893245,
+        picId: 1,
+        dateCreated: '01.10.2020',
+        statusActive: true
+    }
+];
+
+module.exports = users;
