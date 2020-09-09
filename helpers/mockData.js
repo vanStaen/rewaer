b3e3c09fe71f4297b0a3bb39ce3e48deeaa0e669
cvanstaen <clement.van.staen@zalando.de>
2020-09-09 11:37:03 +0200

    Add mock daten

--- helpers/mockData.js
+++ helpers/mockData.js
@@ -1,7 +1,52 @@
+/* rewaer_pictures */
 const pics = [
-    { id: 0, pic: 'pic1.jpg' },
-    { id: 1, pic: 'pic2.jpg' },
-    { id: 2, pic: 'pic3.jpg' }
+    {
+        id: 0,
+        originalFileName: 'test',
+        extension: 'jpg',
+        url: 'pic1.jpg',
+        dateAdded: '01.10.2020'
+    },
+    {
+        id: 1,
+        originalFileName: 'test',
+        extension: 'jpg',
+        url: 'pic2.jpg',
+        dateAdded: '01.10.2020'
+    },
+    {
+        id: 2,
+        originalFileName: 'test',
+        extension: 'jpg',
+        url: 'pic3.jpg',
+        dateAdded: '01.10.2020'
+    }
 ];
+
+/* rewaer_pictures */
+const users = [
+    {
+        id: 1,
+        dateCreated: '01.10.2020',
+        userName: 1,
+        googleId: 1,
+        picId: 1,
+        statusActive: true
+    }
+];
+
+/* rewaer_item */
+const items = [
+    {
+        id: 1,
+        dateCreated: '01.10.2020',
+        brandId: 1,
+        colorMainCode: 1,
+        colorSecondCode: 2,
+        categoryId: 3,
+        flagOnline: true
+    }
+];
+
 
 module.exports = pics;
