cc7e19aad63ad917cc0e77707ff15d7d27beb58f
cvanstaen <clement.van.staen@zalando.de>
2020-09-10 12:05:02 +0200

    protecting lopin infos

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const path = require('path');
 const logger = require('./helpers/logger')
 const PORT = process.env.PORT || 5000;
 
+/* var CONFIG = require('./config.json');
+var dbPort = CONFIG.dbPort;
+var dbHost = CONFIG.dbHost;
+var dbName = CONFIG.dbName;
+ */
+
 // Init Express
 const app = express();
 
@@ -22,3 +28,4 @@ app.use('/api/users', require('./api/users'));
 
 // Listen on a port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
