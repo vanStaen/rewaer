7df7b73512b8c79fc29254d410f11079f7016130
Clément van Staen <clementvanstaen@Clements-iMac.local>
2020-09-13 23:31:58 +0200

    .log to gitignore

--- helpers/logger.js
+++ helpers/logger.js
@@ -8,7 +8,7 @@ const logger = (req, res, next) => {
     const url = req.protocol + '://' + req.get('host') + req.originalUrl;
     const logLine = moment().format("DD/MM/YYYY, H:mm:ss") + ' > ' + req.method + ': ' + url + ' (id: ' + uuid.v4() + ')\r\n';
 
-    fs.appendFile(path.join(__dirname, '../public/', 'log.txt'), logLine, 'utf8', err => {
+    fs.appendFile(path.join(__dirname, '../', 'routes.log'), logLine, 'utf8', err => {
         if (err) throw err;
     });
 
