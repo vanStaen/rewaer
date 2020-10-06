baa88d0af75a709c02c0ca03d5466852613ff7bc
Clément van Staen <clementvanstaen@Clements-iMac.local>
2020-10-06 19:43:34 +0200

    using filtering when reading data

--- references/monogodb/readDataFromMongodb.js
+++ references/monogodb/readDataFromMongodb.js
@@ -10,18 +10,15 @@ async function read() {
         const database = client.db("rewear");
         const collection = database.collection("users");
 
-        /* query for movies that have a runtime less than 15 minutes
-        const query = { runtime: { $lt: 15 } };
+        // query for user that have a userID greater than 0
+        const query = { userID: { $gte: 0 } };
 
         const options = {
-            // sort returned documents in ascending order by title (A->Z)
-            sort: { title: 1 },
-            // Include only the `title` and `imdb` fields in each returned document
-            projection: { _id: 0, title: 1, imdb: 1 },
+            // sort returned documents in ascending order by userID
+            sort: { userID: 1 }
         };
 
-        const cursor = collection.find(query, options);*/
-        const cursor = collection.find();
+        const cursor = collection.find(query, options);
 
         // print a message if no documents were found
         if ((await cursor.count()) === 0) {
