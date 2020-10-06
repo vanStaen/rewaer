ff9204e6e505e2b6e98aa9ea99cb4ada48e8271f
Clément van Staen <clementvanstaen@Clements-iMac.local>
2020-10-06 11:50:43 +0200

    get users as empty array

--- readmongodb.js
+++ readmongodb.js
@@ -2,7 +2,7 @@ const { MongoClient } = require('mongodb');
 const dbConfigInfo = require('./config.db.json');
 
 async function main() {
-    const uri = "mongodb+srv://" + dbConfigInfo.user + ":" + dbConfigInfo.password + "@" + dbConfigInfo.host + "/" + dbConfigInfo.database + "?retryWrites=true&w=majority"
+    const uri = "mongodb+srv://" + dbConfigInfo.user + ":" + dbConfigInfo.password + "@" + dbConfigInfo.host + "/" + dbConfigInfo.database + "?retryWrites=true&w=majority&useUnifiedTopology=true"
     const client = new MongoClient(uri);
 
     try {
@@ -11,6 +11,7 @@ async function main() {
 
         // Make the appropriate DB calls
         await listDatabases(client);
+        await listUsers(client);
 
     } catch (e) {
         console.error(e);
@@ -21,9 +22,14 @@ async function main() {
 
 async function listDatabases(client) {
     databasesList = await client.db().admin().listDatabases();
-
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function listUsers(client) {
+    console.log("Users:");
+    const users = await client.db("rewaer").collection('users').find({}).toArray();
+    console.log(users);
 };
 
 // Run main
