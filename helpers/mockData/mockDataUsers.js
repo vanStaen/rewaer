a46fd6fa7d8cd1725c75d9e874ce115541f2f280
Clément van Staen <clement.vanstaen@gmail.com>
2020-10-07 13:03:41 +0200

    update mock data to reflect reality

--- helpers/mockData/mockDataUsers.js
+++ helpers/mockData/mockDataUsers.js
@@ -1,21 +1,27 @@
 /* rewaer_users */
 const users = [
     {
-        id: 0,
-        userName: 'Clément',
-        googleId: 10002030432532450,
-        picId: 0,
-        dateCreated: '01.10.2020',
-        statusActive: true
-    },
-
-    {
-        id: 1,
-        userName: 'Theresa',
-        googleId: 12342549080893245,
-        picId: 1,
-        dateCreated: '01.10.2020',
-        statusActive: true
+        "_id": {
+            "$oid": "5f7b6eb267bc3d80cd193d0e"
+        },
+        "userID": 0,
+        "name": "Clément",
+        "email": "clement.vanstaengmail.com",
+        "joinDate": "03-09-2019",
+        "encryptedPWD": "61cf1385b1755bbee51cfc02400b1825",
+        "avatar": "1.jpg",
+        "active": true
+    }, {
+        "_id": {
+            "$oid": "5f7b703967bc3d80cd193d10"
+        },
+        "userID": 1,
+        "name": "Theresa",
+        "email": "theresa.pogge@gmail.com",
+        "joinDate": "03-09-2019",
+        "encryptedPWD": "931dd0ac6207488f6e0a63acd7eb977e",
+        "avatar": "2.jpg",
+        "active": true
     }
 ];
 
