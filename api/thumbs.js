196b020d6346dea1de09bd4e99f3f347d19804cd
Clément van Staen <clement.vanstaen@gmail.com>
2020-11-24 21:24:11 +0100

    wip thumbs

--- api/thumbs.js
+++ api/thumbs.js
@@ -6,58 +6,58 @@ const { v1: uuidv1 } = require('uuid');
 const router = express.Router();
 
 
-// Define s23 bucket login info
+// Define S3 bucket login info
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_IAM_KEY,
   secretAccessKey: process.env.AWS_IAM_SECRET_KEY,
   Bucket: process.env.S3_BUCKET_ID
 });
 
-// Upload File to S3
-async function uploadFileS3(tempURL, tempFileName) {
-  const file = fs.readFileSync(tempURL);
-  const params = {
-    Bucket: process.env.S3_BUCKET_ID,
-    Key: tempFileName,
-    Body: file
-  };
-  // Uploading files to the bucket
-  const resURL = await s3.upload(params, function (err, data) {
-    if (err) {
-      throw err;
-    }
-    console.log(`File uploaded successfully. ${data.Location}`);
-    return data.Location
-  });
-  return resURL
-};
 
 // Post a URL of a pic and get a URL of its thumb on AWS S3
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
 
   const url = req.body.url;
   const tempPath = './public/uploads/';
   const tempFileName = uuidv1();
-  const tempURL = tempPath + tempFileName;
-  const cleanedUrl = tempURL.slice(2, tempURL.length);
 
-  jimp.read(url, (err, img) => {
-    if (err) {
-      res.status(400).json({
-        error: `${err})`,
-      });
-      return;
-    }
-    img.resize(120, 120)
-      .quality(60)
-      .write(tempURL);
+  const createThumbPic = async () => {
+    const tempURL = tempPath + tempFileName;
+    jimp.read(url, async (err, img) => {
+      if (err) {
+        res.status(400).json({
+          error: `${err})`,
+        });
+        return;
+      }
+      img.resize(120, 120)
+        .quality(60)
+        .write(tempURL);
+    });
+    return tempURL;
+  }
 
-    // upload to S3
-    const thumbOnS3 = uploadFileS3(tempURL, tempFileName);
-    console.log('thumbOnS3', thumbOnS3)
+  const uploadThumbnailToS3 = async () => {
+    const tempURLThumb = await createThumbPic();
+    const file = await fs.readFileSync(tempURLThumb);
+    const params = {
+      Bucket: process.env.S3_BUCKET_ID,
+      Key: tempFileName,
+      Body: file
+    };
+    // Uploading files to the bucket
+    await s3.upload(params, function (err, data) {
+      if (err) { throw err; }
+      console.log(`File uploaded successfully. ${data.Location}`);
+    });
+    return Data.Location;
+  }
 
-    // Return the URL of the tubnail on S3
-    res.status(201).json({ thumbUrlOns3: `http://localhost:5000/${thumbOnS3}` });
+  // Execute Code and respond to client
+  uploadThumbnailToS3().then((resData) => {
+    res.status(201).json({ thumbUrl: resData });
+  }).catch((err) => {
+    res.status(400).json({ error: err });
   });
 
 });
