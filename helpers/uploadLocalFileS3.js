82eb68973bcdf75fc4867267010e948e4096b1c0
Clément van Staen <clement.vanstaen@gmail.com>
2020-11-25 20:45:25 +0100

    final function upload to s3

--- helpers/uploadLocalFileS3.js
+++ helpers/uploadLocalFileS3.js
@@ -18,21 +18,24 @@ const uploadLocalFileS3 = async (fileUrlLocal, key) => {
         Body: file
     };
 
+
     // Uploading files to the bucket
-    const url = await s3.upload(params, function (err, data) {
+    await s3.upload(params, function (err, data) {
         if (err) { throw err; }
-        //console.log(`File uploaded successfully. ${data.Location}`);
-        return data.Location;
+        // console.log(`File uploaded successfully. ${data.Location}`);
     });
+
+    const url = `https://${process.env.S3_BUCKET_ID}.s3.eu-central-1.amazonaws.com/${key}`
 
     return url;
 
 }
 
-//module.exports = uploadLocalFileS3;
+module.exports = uploadLocalFileS3;
+
 
 /*
-### SOLUTION : THEN/CATCH ### */
+### SOLUTION 1 : THEN/CATCH
 
 uploadLocalFileS3('./public/uploads/d7894ac0-2f45-11eb-af5b-1def7dbe0c26', 'd7894ac0-2f45-11eb-af5b-1def7dbe0c26')
     .then(finalURL => {
@@ -40,6 +43,15 @@ uploadLocalFileS3('./public/uploads/d7894ac0-2f45-11eb-af5b-1def7dbe0c26', 'd789
         console.log(finalURL);
     });
 
+### SOLUTION 2 : AWAIT/SYNC
 
+async function testrun() {
+    const finalURL = await uploadLocalFileS3('./public/uploads/d7894ac0-2f45-11eb-af5b-1def7dbe0c26', 'd7894ac0-2f45-11eb-af5b-1def7dbe0c26')
+    console.log('Wait until it happens');
+    console.log(finalURL);
+}
+testrun();
+
+*/
 
 
