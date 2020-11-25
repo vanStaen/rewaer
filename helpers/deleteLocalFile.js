0c5683caf70619fe8a94139cd5cc9e3c205d2ed9
Clément van Staen <clement.vanstaen@gmail.com>
2020-11-25 21:46:27 +0100

    delete file helper function

---
+++ helpers/deleteLocalFile.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const deleteLocalFile = async (fileName) => {
+
+    const root = './public/uploads/';
+    const path = root + fileName;
+    console.log(path);
+
+    try {
+        fs.unlinkSync(path);
+        console.log(`File ${fileName} successfully deleted`);
+    } catch (err) {
+        console.log('error', err);
+    }
+}
+
+module.exports = deleteLocalFile;
