9b285565d9b6a38987c43b23a3063ca0e216bade
Clément van Staen <clement.vanstaen@gmail.com>
2020-11-26 20:37:24 +0100

    upload endpoint with thumbnail

--- api/upload.js
+++ api/upload.js
@@ -3,6 +3,11 @@ const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const router = express.Router();
+const { v1: uuidv1 } = require('uuid');
+
+const createThumbnail = require('../helpers/createThumbnail')
+const uploadFileFromUrlToS3 = require('../helpers/uploadFileFromUrlToS3')
+const deleteLocalFile = require('../helpers/deleteLocalFile')
 
 // Limits size of 5MB
 const sizeLimits = { fileSize: 1024 * 1024 * 5 };
@@ -34,6 +39,51 @@ const uploadS3 = multer({
   fileFilter: fileFilter
 }).single('file');
 
+router.post('/', async (req, res, next) => {
+  if (!req.isAuth)
+    return res.status(401).json({ error: "Unauthenticated" });
+  next();
+}, (req, res) => {
+  uploadS3(req, res, (error) => {
+    //console.log('Requested File: ', req.file);
+    if (error) {
+      console.log('errors', error);
+      res.json({ error: error });
+    } else {
+      // If File not found
+      if (req.file === undefined) {
+        console.log('Error: No File Selected!');
+        res.json('Error: No File Selected');
+      } else {
+        // If Success
+        const imageOriginalName = req.file.originalname;
+        const imageUrl = req.file.location;
+        const thumbName = "t_" + req.file.key;
+        // Create Thumbnail
+        createThumbnail(imageUrl, thumbName)
+          .then(thumbUrlLocal => {
+            uploadFileFromUrlToS3(thumbUrlLocal, thumbName)
+              .then(thumbUrlS3 => {
+                deleteLocalFile(thumbName);
+                // Return file name and file url to client
+                return res.status(200).json({
+                  imageOriginalName: imageOriginalName,
+                  imageUrl: imageUrl,
+                  thumbUrl: thumbUrlS3
+                });
+              }).catch((err) => {
+                return res.status(400).json({ error: err });
+              });
+          });
+      }
+    }
+  });
+});
+
+module.exports = router;
+
+/*
+
 router.post('/', (req, res, next) => {
   if (!req.isAuth)
     return res.status(401).json({ error: "Unauthenticated" });
@@ -63,4 +113,4 @@ router.post('/', (req, res, next) => {
   });
 });
 
-module.exports = router;
+*/
