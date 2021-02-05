8e1b409a06dc93296d8b50d36f903202114eacfc
Clément van Staen <clement.vanstaen@gmail.com>
2021-02-05 20:23:25 +0100

    thumbnail on all picture upload

--- api/upload.js
+++ api/upload.js
@@ -3,6 +3,7 @@ const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const router = express.Router();
+const fs = require('fs');
 
 const createThumbnail = require('../helpers/createThumbnail')
 const uploadFileFromUrlToS3 = require('../helpers/uploadFileFromUrlToS3')
@@ -40,6 +41,76 @@ const uploadS3 = multer({
 }).single('file');
 
 
+// DELETE single file object from s3 (based on key)
+router.delete("/:id", async (req, res) => {
+  if (!req.isAuth) {
+    res.status(401).json({
+      error: "Unauthorized",
+    });
+    return;
+  }
+  try {   
+    var params = {  Bucket: process.env.S3_BUCKET_ID, Key: req.params.id };
+    s3.deleteObject(params, function(err, data) {
+      console.log(`Object id ${req.params.id} has been deleted`);
+      res.status(204).json({});
+    });
+  } catch (err) {
+    res.status(400).json({
+      error: `${err}`,
+    });
+  }
+});
+
+
+// POST single file object to s3
+router.post('/', async (req, res, next) => {
+  if (!req.isAuth)
+    return res.status(401).json({ error: "Unauthenticated" });
+  next();
+}, (req, res) => {
+  uploadS3(req, res, (error) => {
+    if (error) {
+      console.log('errors', error);
+      res.json({ error: error });
+    } else {
+      // If File not found
+      if (req.file === undefined) {
+        res.json({ Error: 'No File Selected' });
+      } else {
+        // If Success
+        const imageOriginalName = req.file.originalname;
+        const imageUrl = req.file.location;
+        const thumbName = "t_" + req.file.key;
+        // Create Thumbnail
+        createThumbnail(imageUrl, thumbName)
+          .then(thumbUrlLocal => {
+            fs.watch(thumbUrlLocal, () => { 
+            uploadFileFromUrlToS3(thumbUrlLocal, thumbName)
+              .then(thumbUrlS3 => {
+                deleteLocalFile(thumbName);
+                // Return file name and file url to client
+                return res.status(200).json({
+                  imageOriginalName: imageOriginalName,
+                  imageUrl: imageUrl,
+                  thumbUrl: thumbUrlS3
+                });
+              }).catch((err) => {
+                console.log(err)
+                return res.status(400).json({ error: err });
+              });
+            });
+          });
+      }
+    }
+  });
+});
+
+module.exports = router;
+
+/*
+
+// OLD UPLOAD without thumbnail
 router.post('/', (req, res, next) => {
   if (!req.isAuth)
     return res.status(401).json({ error: "Unauthenticated" });
@@ -69,71 +140,5 @@ router.post('/', (req, res, next) => {
     }
   });
 });
-
-// DELETE single data from songbook (based on id)
-router.delete("/:id", async (req, res) => {
-  if (!req.isAuth) {
-    res.status(401).json({
-      error: "Unauthorized",
-    });
-    return;
-  }
-  try {   
-    var params = {  Bucket: process.env.S3_BUCKET_ID, Key: req.params.id };
-    s3.deleteObject(params, function(err, data) {
-      console.log(`Object id ${req.params.id} has been deleted`);
-      res.status(204).json({});
-    });
-  } catch (err) {
-    res.status(400).json({
-      error: `${err}`,
-    });
-  }
-});
-
-module.exports = router;
-
-/*
-
-router.post('/', async (req, res, next) => {
-  if (!req.isAuth)
-    return res.status(401).json({ error: "Unauthenticated" });
-  next();
-}, (req, res) => {
-  uploadS3(req, res, (error) => {
-    if (error) {
-      console.log('errors', error);
-      res.json({ error: error });
-    } else {
-      // If File not found
-      if (req.file === undefined) {
-        res.json({ Error: 'No File Selected' });
-      } else {
-        // If Success
-        const imageOriginalName = req.file.originalname;
-        const imageUrl = req.file.location;
-        const thumbName = "t_" + req.file.key;
-        // Create Thumbnail
-        createThumbnail(imageUrl, thumbName)
-          .then(thumbUrlLocal => {
-            uploadFileFromUrlToS3(thumbUrlLocal, thumbName)
-              .then(thumbUrlS3 => {
-                deleteLocalFile(thumbName);
-                // Return file name and file url to client
-                return res.status(200).json({
-                  imageOriginalName: imageOriginalName,
-                  imageUrl: imageUrl,
-                  thumbUrl: thumbUrlS3
-                });
-              }).catch((err) => {
-                console.log(err)
-                return res.status(400).json({ error: err });
-              });
-          });
-      }
-    }
-  });
-});
-
 
 */
