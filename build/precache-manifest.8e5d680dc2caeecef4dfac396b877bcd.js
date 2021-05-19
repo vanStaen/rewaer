a1a8f7d58f866502b04e59f6294c553d80ea7b97
Clément van Staen <clement.vanstaen@gmail.com>
2021-05-19 16:09:46 +0200

    deploy frontend Build

---
+++ build/precache-manifest.8e5d680dc2caeecef4dfac396b877bcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "49cc4f0b9cdd73e4a3d5",
+    "url": "/static/css/main.959a8d03.chunk.css"
+  },
+  {
+    "revision": "49cc4f0b9cdd73e4a3d5",
+    "url": "/static/js/main.925bb7d5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "be2c6ccce2b168391cfb",
+    "url": "/static/js/2.1040afaa.chunk.js"
+  },
+  {
+    "revision": "a7b2f84d33d56d4c2392f6f4f7f2209d",
+    "url": "/index.html"
+  }
+];
