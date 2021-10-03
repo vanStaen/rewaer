3a592e493de44703d6331f2958dc1fefb58682ca
vanStaen <clement.vanstaen@gmail.com>
2021-10-03 18:30:48 +0200

    edit settings WIP

--- frontend/src/pages/Profil/EditSettings/EditSettings.jsx
+++ frontend/src/pages/Profil/EditSettings/EditSettings.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { observer } from "mobx-react";
+import { Divider, Switch } from "antd";
+import { CloseOutlined, CheckOutlined } from "@ant-design/icons";
 
 import { MenuBar } from "../../../components/MenuBar/MenuBar";
 import { userStore } from "../../../stores/userStore/userStore";
@@ -12,7 +14,55 @@ export const EditSettings = observer(() => {
   return (
     <div className="EditSettings__main">
       <MenuBar />
-      <div className="EditSettings__container">EDIT USER SETTINGS</div>
+      <div className="EditSettings__container">
+        <div className="EditSettings__title">Edit your settings on Rewær</div>
+        <br />
+        <Divider orientation="left" plain>
+          Profil Settings
+        </Divider>
+        <div className="EditSettings__singleSetting">
+          <Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            defaultChecked
+          />{" "}
+          Show in my profil when I was last seen online
+        </div>
+        <div className="EditSettings__singleSetting">
+          <Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+          />{" "}
+          Hide my account to anyone which is not my friend
+        </div>
+        <br />
+        <Divider orientation="left" plain>
+          Email Settings
+        </Divider>
+        <div className="EditSettings__singleSetting">
+          <Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            defaultChecked
+          />{" "}
+          Send me a mail when I get a friend request
+        </div>
+        <div className="EditSettings__singleSetting">
+          <Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            defaultChecked
+          />{" "}
+          Send me a mail when I get a new message
+        </div>
+        <div className="EditSettings__singleSetting">
+          <Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+          />{" "}
+          Keep me informed about all changes hapenning with Rewær
+        </div>
+      </div>
     </div>
   );
 });
