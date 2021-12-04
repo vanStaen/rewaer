fe4cb3bc9668a2b610fe75b4bc8fbfd40533ccdd
vanStaen <clement.vanstaen@gmail.com>
2021-12-04 16:32:05 +0100

    use custom card on items

--- frontend/src/pages/items/ItemCard/ItemCard.jsx
+++ frontend/src/pages/items/ItemCard/ItemCard.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { Image, Card, notification, Spin, Popconfirm } from "antd";
-import { DeleteOutlined, QuestionCircleOutlined } from "@ant-design/icons";
+import { notification, Spin, Popconfirm, Tooltip } from "antd";
+import {
+  DeleteOutlined,
+  ExclamationCircleOutlined,
+  HeartOutlined,
+  EditOutlined,
+  EyeOutlined,
+  StarOutlined,
+} from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
 
 import { EditableTitle } from "../../../components/EditableTitle/EditableTitle";
@@ -9,12 +16,10 @@ import { deleteItem } from "./deleteItem";
 
 import "./ItemCard.css";
 
-const { Meta } = Card;
-
 export const ItemCard = (props) => {
   const { t } = useTranslation();
   const spinnerFormated = (
-    <div className="card__spinner">
+    <div className="item__spinner">
       <Spin size="middle" />
     </div>
   );
@@ -36,42 +41,105 @@ export const ItemCard = (props) => {
         console.log(error.message);
       });
   };
+  const elementPicture = document.getElementById(
+    `item_look_picture_${props.item._id}`
+  );
+
+  const elementLogoOver = document.getElementById(
+    `item_look_logoover_${props.item._id}`
+  );
+
+  const elementActionsContainer = document.getElementById(
+    `item_look_actionsContainer_${props.item._id}`
+  );
+
+  const elementActionsLogo = document.getElementById(
+    `item_look_actionsLogo_${props.item._id}`
+  );
+
+  const onMouseEnterHandler = () => {
+    elementPicture.style.filter = "brightness(50%)";
+    elementLogoOver.style.display = "block";
+    elementActionsContainer.style.width = "34px";
+    elementActionsContainer.style.opacity = ".85";
+    elementActionsLogo.style.display = "block";
+  };
+
+  const onMouseLeaveHandler = () => {
+    elementPicture.style.filter = "brightness(100%)";
+    elementLogoOver.style.display = "none";
+    elementActionsContainer.style.width = "0px";
+    setTimeout(() => {
+      elementActionsLogo.style.display = "none";
+      elementActionsContainer.style.opacity = "0";
+    }, 100);
+  };
+
+  const createdDate = new Date(props.item.createdAt);
 
   return (
-    <Card
-      hoverable
-      bordered
-      style={{ width: 240, marginBottom: 30, height: 385 }}
-      cover={
-        <Image
-          alt={props.item.title}
-          src={props.item.mediaUrl}
-          placeholder={spinnerFormated}
-          width={240}
-          height={320}
-        />
-      }
-    >
-      <Meta
-        title={
-          <div>
-            <EditableTitle
-              title={props.item.title}
-              id={props.item._id}
-              type={"item"}
-            />
-            <Popconfirm
-              title={t("items.deleteConfirm")}
-              onConfirm={handleDelete}
-              okText="Delete"
-              cancelText="Cancel"
-              icon={<QuestionCircleOutlined style={{ color: "red" }} />}
-            >
-              <DeleteOutlined />
-            </Popconfirm>
+    <>
+      <div
+        className="itemcard__container"
+        onMouseEnter={onMouseEnterHandler}
+        onMouseLeave={onMouseLeaveHandler}
+      >
+        <div
+          className="itemcard__picture"
+          id={`item_look_picture_${props.item._id}`}
+          //placeholder={spinnerFormated}
+          style={{
+            background: `url(${props.item.mediaUrlMedium})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div>
+        <div
+          className="itemcard__logoover"
+          id={`item_look_logoover_${props.item._id}`}
+        >
+          <EyeOutlined />
+          <div style={{ fontSize: "12px" }}>Detail View</div>
+        </div>
+        <div
+          className="itemcard__actionsContainer"
+          id={`item_look_actionsContainer_${props.item._id}`}
+        >
+          <div
+            className="itemcard__actionsLogo"
+            id={`item_look_actionsLogo_${props.item._id}`}
+          >
+            <Tooltip placement="left" title="Mark as favorite">
+              <StarOutlined className="iconGold" />
+            </Tooltip>
+            <Tooltip placement="left" title="Edit this Look">
+              <EditOutlined className="iconGreen" />
+            </Tooltip>
+            <Tooltip placement="left" title="Delete this Look">
+              <Popconfirm
+                title={t("looks.deleteConfirm")}
+                onConfirm={handleDelete}
+                okText="Delete"
+                cancelText="Cancel"
+                icon={<ExclamationCircleOutlined style={{ color: "black" }} />}
+              >
+                <DeleteOutlined className="iconRed" />
+              </Popconfirm>
+            </Tooltip>
           </div>
-        }
-      />
-    </Card>
+        </div>
+        <div className="itemcard__meta">
+          <EditableTitle
+            title={props.item.title}
+            id={props.item._id}
+            type={"look"}
+          />
+          <div className="itemcard__date">
+            {createdDate.toLocaleDateString()}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
