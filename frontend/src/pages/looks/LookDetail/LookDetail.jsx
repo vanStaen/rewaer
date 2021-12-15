ea20c1b023184bdf426cfbdc973591feae8e218a
vanStaen <clement.vanstaen@gmail.com>
2021-12-15 13:45:11 +0100

    LookDetaiWIP

--- frontend/src/pages/looks/LookDetail/LookDetail.jsx
+++ frontend/src/pages/looks/LookDetail/LookDetail.jsx
@@ -3,23 +3,29 @@ import { notification, Spin, Popconfirm, Tooltip } from "antd";
 import { ArrowLeftOutlined } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
 
+import "./LookDetail.css";
+
 export const LookDetail = (props) => {
   const { t } = useTranslation();
   return (
     <>
-      <ArrowLeftOutlined
-        style={{ fontSize: "30px", padding: "14px" }}
-        onClick={() => {
-          props.setSelectedLook(null);
-        }}
-      />
-      {props.selectedLook._id}
+      <div className="lookdetail__header">
+        <ArrowLeftOutlined
+          style={{ fontSize: "30px" }}
+          onClick={() => {
+            props.setSelectedLook(null);
+          }}
+        />
+        <div className="lookdetail__headerTitle">
+          {props.selectedLook._id} - {props.selectedLook.title}
+        </div>
+      </div>
 
       <div
-        className="lookcard__picture"
+        className="lookdetail__picture"
         id={`selected_look_picture_${props.selectedLook._id}`}
         style={{
-          background: `url(${props.selectedLook.mediaUrlMedium})`,
+          background: `url(${props.selectedLook.mediaUrl})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
