37417c14b1c72d3487b1cbc409e05e9912878bab
vanStaen <clement.vanstaen@gmail.com>
2021-12-31 19:57:10 +0100

    small refactoring

--- frontend/src/pages/items/ItemDetail/ItemDetail.jsx
+++ frontend/src/pages/items/ItemDetail/ItemDetail.jsx
@@ -4,14 +4,14 @@ import { observer } from "mobx-react";
 import { ArrowLeftOutlined } from "@ant-design/icons";
 import { useTranslation } from "react-i18next";
 
-import { ItemDetailFormCategory } from "./ItemDetailFormElement/ItemDetailFormCategory";
+import { ItemDetailFormDropDown } from "./ItemDetailFormElement/ItemDetailFormDropDown";
 import { ItemDetailFormStringElement } from "./ItemDetailFormElement/ItemDetailFormStringElement";
 import { itemsStore } from "../itemsStore";
 import { userStore } from "../../../stores/userStore/userStore";
 import {
   itemCategoryMen,
   itemCategoryWomen,
-  itemCategoryNB
+  itemCategoryNB,
 } from "../../../data/categories";
 
 import { colors } from "../../../data/colors";
@@ -58,56 +58,66 @@ export const ItemDetail = observer((props) => {
         ></div>
       </div>
 
-      {
-        itemsStore.isLoading ?
-          <div className="itemDetail__itemContainer">
-            <div className="itemDetail__spinner">
-              <Spin size="large" />
-            </div>
+      {itemsStore.isLoading ? (
+        <div className="itemDetail__itemContainer">
+          <div className="itemDetail__spinner">
+            <Spin size="large" />
           </div>
-          :
-          <div className="itemDetail__itemContainer">
-            <ItemDetailFormStringElement
-              element="title"
-              title="title"
-              value={props.selectedItem.title}
-              selectedItem={props.selectedItem} />
-            <ItemDetailFormCategory
-              title="category"
-              element="category"
-              data={
-                userStore.gender === 1 ?
-                  itemCategoryMen :
-                  userStore.gender === 2 ?
-                    itemCategoryWomen :
-                    itemCategoryNB
-              }
-              value={props.selectedItem.category}
-              selectedItem={props.selectedItem} />
-            <ItemDetailFormStringElement
-              element="desc"
-              title="description"
-              value={props.selectedItem.desc}
-              selectedItem={props.selectedItem} />
-            <ItemDetailFormCategory
-              title="colors"
-              element="colors"
-              data={colors}
-              value={null}
-              selectedItem={props.selectedItem} />
-            <ItemDetailFormCategory
-              title="pattern"
-              element="pattern"
-              data={pattern}
-              value={null}
-              selectedItem={props.selectedItem} />
-            brand<br />
-            active/archived<br />
-            favorite<br />
-            private<br />
-            status - 0: There, 1: Sold, 2: Thrown, 3: GivenAway, 4: Lent, 5: Lost, 6: ForSale<br />
-          </div>
-      }
-    </div >
+        </div>
+      ) : (
+        <div className="itemDetail__itemContainer">
+          <ItemDetailFormStringElement
+            element="title"
+            title="title"
+            value={props.selectedItem.title}
+            selectedItem={props.selectedItem}
+          />
+          <ItemDetailFormDropDown
+            title="category"
+            element="category"
+            data={
+              userStore.gender === 1
+                ? itemCategoryMen
+                : userStore.gender === 2
+                ? itemCategoryWomen
+                : itemCategoryNB
+            }
+            value={props.selectedItem.category}
+            selectedItem={props.selectedItem}
+          />
+          <ItemDetailFormStringElement
+            element="desc"
+            title="description"
+            value={props.selectedItem.desc}
+            selectedItem={props.selectedItem}
+          />
+          <ItemDetailFormDropDown
+            title="colors"
+            element="colors"
+            data={colors}
+            value={null}
+            selectedItem={props.selectedItem}
+          />
+          <ItemDetailFormDropDown
+            title="pattern"
+            element="pattern"
+            data={pattern}
+            value={null}
+            selectedItem={props.selectedItem}
+          />
+          brand
+          <br />
+          active/archived
+          <br />
+          favorite
+          <br />
+          private
+          <br />
+          status - 0: There, 1: Sold, 2: Thrown, 3: GivenAway, 4: Lent, 5: Lost,
+          6: ForSale
+          <br />
+        </div>
+      )}
+    </div>
   );
 });
---
+++ frontend/src/pages/items/ItemDetail/ItemDetailFormElement/ItemDetailFormDropDown.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { Dropdown, Menu, notification } from "antd";
+import { useTranslation } from "react-i18next";
+import { QuestionCircleOutlined } from "@ant-design/icons";
+
+import { updateGenericStringItem } from "../../actions/updateGenericStringItem";
+
+import "./ItemDetailFormElement.css";
+
+export const ItemDetailFormDropDown = (props) => {
+  const { t } = useTranslation();
+  const [value, setValue] = useState(props.value);
+
+  const clickHandler = (newValue) => {
+    try {
+      updateGenericStringItem(props.selectedItem._id, props.element, newValue);
+      setValue(newValue);
+      notification.success({
+        message: t("main.changeSaved"),
+        placement: "bottomRight",
+      });
+      itemsStore.setIsOutOfDate(true);
+    } catch (e) {
+      notification.error({
+        message: e,
+        placement: "bottomRight",
+      });
+    }
+  };
+
+  const CategoryDropDown = props.data.map((item) => {
+    return (
+      <Menu.Item
+        key={item.code}
+        //style={{ backgroundColor: item.en }}
+        onClick={() => {
+          clickHandler(item.en);
+        }}
+      >
+        {item.en}
+      </Menu.Item>
+    );
+  });
+
+  return (
+    <div className="ItemDetailFormElement__container">
+      <div className="ItemDetailFormElement__title">{props.title}:</div>
+      <Dropdown
+        className="ItemDetailFormElement__dropdown"
+        overlay={<Menu>{CategoryDropDown}</Menu>}
+        placement="bottomLeft"
+      >
+        <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
+          {props.value ? (
+            <span className="ItemDetailFormElement__element">{value}</span>
+          ) : (
+            <span className="ItemDetailFormElement__selectElement">
+              Select a {props.title}
+            </span>
+          )}
+        </a>
+      </Dropdown>
+      <div className="ItemDetailFormElement__helpIcon">
+        <QuestionCircleOutlined />
+      </div>
+    </div>
+  );
+};
