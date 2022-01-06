9863f5b01dfd7d6bb8bccc99e76c3d0b93d15bc6
vanStaen <clement.vanstaen@gmail.com>
2022-01-06 21:50:35 +0100

    prettier

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -3,11 +3,7 @@ import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { observer } from "mobx-react";
 import { Spin } from "antd";
-import {
-  CameraOutlined,
-  SkinOutlined,
-  MehOutlined,
-} from "@ant-design/icons";
+import { CameraOutlined, SkinOutlined, MehOutlined } from "@ant-design/icons";
 
 import { userStore } from "../../stores/userStore/userStore";
 import { profileStore } from "../../stores/profileStore/profileStore";
@@ -29,7 +25,7 @@ export const Profile = observer(() => {
   useEffect(() => {
     const username = params.username ? params.username : userStore.userName;
     profileStore.fetchProfileData(username);
-  }, [userStore.isLoading])
+  }, [userStore.isLoading]);
 
   return (
     <div className="profil__main">
@@ -45,50 +41,56 @@ export const Profile = observer(() => {
           <MehOutlined style={{ fontSize: "120px", color: "#b6c8bf" }} />
         </div>
       ) : (
-            <div className="profil__container">
-              <div className="profil__containerLeft">
-                <Avatar />
-                <ProfileDetails />
-                <ProfileActions />
+        <div className="profil__container">
+          <div className="profil__containerLeft">
+            <Avatar />
+            <ProfileDetails />
+            <ProfileActions />
+          </div>
+          <div className="profil__containerCenter">
+            <div className="profil__subMenuContainer">
+              <div
+                className={`profil__subMenuItem ${
+                  contentToDisplay === "looks" && "profil__subMenuItemSelected"
+                }`}
+                onClick={() => {
+                  setContentToDisplay("looks");
+                  profileStore.setFilterIsPopingUp(true);
+                }}
+              >
+                <CameraOutlined />
+                <span className="profil__counter">
+                  {profileStore.looks && profileStore.looks.length}&nbsp;
+                  {t("menu.looks")}
+                </span>
               </div>
-              <div className="profil__containerCenter">
-                <div className="profil__subMenuContainer">
-                  <div
-                    className={`profil__subMenuItem ${contentToDisplay === "looks" && "profil__subMenuItemSelected"}`}
-                    onClick={() => {
-                      setContentToDisplay("looks");
-                      profileStore.setFilterIsPopingUp(true);
-                    }}
-                  >
-                    <CameraOutlined />
-                    <span className="profil__counter">
-                      {profileStore.looks && profileStore.looks.length}&nbsp;{t("menu.looks")}
-                    </span>
-                  </div>
-                  <div
-                    className={`profil__subMenuItem ${contentToDisplay === "items" && "profil__subMenuItemSelected"}`}
-                    onClick={() => {
-                      setContentToDisplay("items");
-                      profileStore.setFilterIsPopingUp(true);
-                    }}
-                  >
-                    <SkinOutlined />
-                    <span className="profil__counter">
-                      {profileStore.items && profileStore.items.length}&nbsp;{t("menu.items")}
-                    </span>
-                  </div>
-                </div>
-                <div className="profil__containerCenterContent">
-                  {contentToDisplay === "items" && <ProfileItems />}
-                  {contentToDisplay === "looks" && <ProfileLooks />}
-                </div>
-              </div>
-              <div className="profil__containerRight">
-                <ProfileFilters contentToDisplay={contentToDisplay} />
-                <ProfileFriends />
+              <div
+                className={`profil__subMenuItem ${
+                  contentToDisplay === "items" && "profil__subMenuItemSelected"
+                }`}
+                onClick={() => {
+                  setContentToDisplay("items");
+                  profileStore.setFilterIsPopingUp(true);
+                }}
+              >
+                <SkinOutlined />
+                <span className="profil__counter">
+                  {profileStore.items && profileStore.items.length}&nbsp;
+                  {t("menu.items")}
+                </span>
               </div>
             </div>
-          )}
+            <div className="profil__containerCenterContent">
+              {contentToDisplay === "items" && <ProfileItems />}
+              {contentToDisplay === "looks" && <ProfileLooks />}
+            </div>
+          </div>
+          <div className="profil__containerRight">
+            <ProfileFilters contentToDisplay={contentToDisplay} />
+            <ProfileFriends />
+          </div>
+        </div>
+      )}
     </div>
   );
 });
