df2037d5a0d7561e29e554e58451d28743bded48
vanStaen <clement.vanstaen@gmail.com>
2022-01-25 15:14:37 +0100

    footer WIP

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -6,6 +6,8 @@ export const Footer = () => {
     <div className="footerContainer">
       <div className="footerSubContainer">
         Contact | Impressum | About | Newsletter | Cookie
+        <div>Social Media Logos</div>
+        <div>Copyright ©{new Date().getFullYear()} </div>
       </div>
     </div>
   );
