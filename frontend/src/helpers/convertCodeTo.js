5885327ced90ead2c3d6e09d0ab77307f5ff49fc
vanStaen <clement.vanstaen@gmail.com>
2022-03-07 14:28:15 +0100

    restore helpers folder

---
+++ frontend/src/helpers/convertCodeTo.js
@@ -0,0 +1,12 @@
+export const convertCodeToObjectString = (searchForCode, searchInObject) => {
+    const result = searchInObject.find((element) => searchForCode === element.code);
+    if (result){
+      return result
+    }
+    else {
+      console.log("searchForCode", searchForCode);
+      console.log("searchInObject", searchInObject);
+      return { en: "not found", de: "not found", fr: "not found"}
+    }
+ };
+ 
