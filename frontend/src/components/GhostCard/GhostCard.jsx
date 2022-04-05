6edcfb66f8c1c479227c842f832f52cb161e079b
vanStaen <clement.vanstaen@gmail.com>
2022-04-05 11:40:57 +0200

    margin ghostcard bug fix

--- frontend/src/components/GhostCard/GhostCard.jsx
+++ frontend/src/components/GhostCard/GhostCard.jsx
@@ -4,6 +4,7 @@ import { Col } from "antd";
 import "./GhostCard.css";
 
 export const GhostCard = (props) => {
+  const margin = props.margin ? props.margin : [0, 0, 0, 0];
   let ghost = [];
   for (let i = 0; i < props.numberOfCards; i++) {
     ghost.push(
@@ -13,10 +14,10 @@ export const GhostCard = (props) => {
           style={{
             width: props.width,
             height: props.height,
-            marginTop: props.margin[0],
-            marginRight: props.margin[1],
-            marginBottom: props.margin[2],
-            marginLeft: props.margin[3],
+            marginTop: margin[0],
+            marginRight: margin[1],
+            marginBottom: margin[2],
+            marginLeft: margin[3],
           }}
         ></div>
       </Col>
