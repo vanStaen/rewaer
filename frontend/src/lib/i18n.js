39103ba1d69f7742ef5cbc5237e4fab2d38935a1
vanStaen <clement.vanstaen@gmail.com>
2022-04-19 14:18:21 +0200

    bo debugging in the console for i18n

--- frontend/src/lib/i18n.js
+++ frontend/src/lib/i18n.js
@@ -1,6 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { KnownFragmentNamesRule } from 'graphql';
 
 const dataEn = require('./languages/en/translation.json');
 const dataDe = require('./languages/de/translation.json');
@@ -11,7 +12,7 @@ i18n
   .use(initReactI18next)
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    debug: true,
+    debug: KnownFragmentNamesRule,
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
