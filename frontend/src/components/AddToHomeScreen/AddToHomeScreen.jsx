be6d8ebb57bc81311d78294b1fb498feef0e3263
vanStaen <clement.vanstaen@gmail.com>
2022-05-17 14:31:57 +0200

    AddToHomeScreen loging

--- frontend/src/components/AddToHomeScreen/AddToHomeScreen.jsx
+++ frontend/src/components/AddToHomeScreen/AddToHomeScreen.jsx
@@ -13,23 +13,18 @@ export const AddToHomeScreen = (props) => {
   }, []);
 
   const installPromptHandler = (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
-    // Stash the event so it can be triggered later.
     deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
+    console.log("A2HS prompt was prevented and stored!");
     a2hs__button.style.display = "block";
   };
 
   const addToHomeScreenClickHandler = () => {
-    // hide our user interface that shows our A2HS button
-    a2hs__button.style.display = "none";
-    // Show the prompt
     deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === "accepted") {
         console.log("User accepted the A2HS prompt");
+        a2hs__button.style.display = "none";
       } else {
         console.log("User dismissed the A2HS prompt");
       }
