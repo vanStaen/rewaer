90525daa3a6efde51515011ce6ca6c1f74009f4f
vanStaen <clement.vanstaen@gmail.com>
2023-09-18 12:18:23 +0200

    change banner color

--- src/components/ToolBar/ToolBar.jsx
+++ src/components/ToolBar/ToolBar.jsx
@@ -9,7 +9,7 @@ import { itemsStore } from "../../pages/Items/itemsStore";
 
 import "./ToolBar.css";
 
-const TRESHOLD_SCROLL_TOOL_BAR_COLOR_CHANGE = 80;
+const TRESHOLD_SCROLL_TOOL_BAR_COLOR_CHANGE = 50;
 
 export const ToolBar = (props) => {
   const { t } = useTranslation();
@@ -34,50 +34,48 @@ export const ToolBar = (props) => {
   }, [scrollhandler]);
 
   return (
-    <>
-      <div className="ToolBar__toolbar" id="toolbar">
-        <div className="ToolBar__toolbarLeft">
-          {props.total}&nbsp;
-          {t(`menu.${page}`)}
-          {page === "looks" && looksStore.numberOfPrivateLook > 0 && (
-            <>
-              {" "}
-              | &nbsp;
-              <span
-                className="link"
-                onClick={() => {
-                  looksStore.setShowPrivateLooks(!looksStore.showPrivateLooks);
-                }}
-              >
-                {looksStore.showPrivateLooks
-                  ? t("looks.hidePrivateLooks")
-                  : t("looks.showPrivateLooks")}
-              </span>
-            </>
-          )}
-          {page === "items" && itemsStore.numberOfPrivateItem > 0 && (
-            <>
-              {" "}
-              |
-              <span
-                className="link"
-                onClick={() => {
-                  itemsStore.setShowPrivateItems(!itemsStore.showPrivateItems);
-                }}
-              >
-                &nbsp;
-                {itemsStore.showPrivateItems
-                  ? t("items.hidePrivateItems")
-                  : t("items.showPrivateItems")}
-              </span>
-            </>
-          )}
-        </div>
-        <div className="ToolBar__toolbarRight">
-          <Edit />
-          <Filter />
-        </div>
+    <div className="ToolBar__toolbar" id="toolbar">
+      <div className="ToolBar__toolbarLeft">
+        {props.total}&nbsp;
+        {t(`menu.${page}`)}
+        {page === "looks" && looksStore.numberOfPrivateLook > 0 && (
+          <>
+            {" "}
+            | &nbsp;
+            <span
+              className="link"
+              onClick={() => {
+                looksStore.setShowPrivateLooks(!looksStore.showPrivateLooks);
+              }}
+            >
+              {looksStore.showPrivateLooks
+                ? t("looks.hidePrivateLooks")
+                : t("looks.showPrivateLooks")}
+            </span>
+          </>
+        )}
+        {page === "items" && itemsStore.numberOfPrivateItem > 0 && (
+          <>
+            {" "}
+            |
+            <span
+              className="link"
+              onClick={() => {
+                itemsStore.setShowPrivateItems(!itemsStore.showPrivateItems);
+              }}
+            >
+              &nbsp;
+              {itemsStore.showPrivateItems
+                ? t("items.hidePrivateItems")
+                : t("items.showPrivateItems")}
+            </span>
+          </>
+        )}
       </div>
-    </>
+      <div className="ToolBar__toolbarRight">
+        <Edit />
+        <Filter />
+      </div>
+    </div>
   );
 };
