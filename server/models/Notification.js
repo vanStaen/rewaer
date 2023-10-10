9f3f24ce33a996e4b6330b168df9850e2df0f9f4
vanStaen <clement.vanstaen@gmail.com>
2023-10-10 22:24:24 +0200

    notifications table

---
+++ server/models/Notification.js
@@ -0,0 +1,56 @@
+const { sequelize, DataTypes } = require('../lib/sequelizedb');
+const { User } = require('./User');
+
+const Notification = sequelize.define("notification", {
+  _id: {
+    type: DataTypes.INTEGER,
+    field: "_id",
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  media_url: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.STRING,
+    required: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    required: false,
+  },
+  action_data: {
+    type: DataTypes.STRING,
+    required: false,
+  },
+  type: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  seen: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = {
+  Notification
+};
+
+/*
+types:
+0: General
+1: Friend request
+2: New Follower
+3: New Messages
+4: New item from friendsfollowed
+5: New look from friend/followed
+6: Someone shared an item with you (theirs)
+7: Someone wants to share an item (yours)
+8: Someone made you a Look
+*/
