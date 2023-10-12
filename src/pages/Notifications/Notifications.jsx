bc6f9755d50aac21274592b8bd2dda81f12da17b
vanStaen <clement.vanstaen@gmail.com>
2023-10-12 14:14:49 +0200

    notifications

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -4,7 +4,7 @@ import { observer } from "mobx-react";
 import { pageStore } from "../../stores/pageStore/pageStore";
 import { postNotificationsSeen } from "./postNotificationsSeen";
 
-import "./Notifications.css";
+import "./Notifications.less";
 
 export const Notifications = observer(() => {
   useEffect(() => {
@@ -15,8 +15,9 @@ export const Notifications = observer(() => {
   const notificationsFormated = pageStore.notifications.map((notification) => {
     const { type, seen, title, createdAt, media_url } = notification;
     return (
-      <div className={seen ? "seen" : "new"}>
+      <div className={`notifications__notification ${seen ? "seen" : "new"}`}>
         <div
+          className="notifications__leftSide"
           style={{
             width: "50px",
             height: "50px",
@@ -26,13 +27,14 @@ export const Notifications = observer(() => {
             backgroundRepeat: "no-repeat",
           }}
         ></div>
-        {type === 1 && <div>{title} sent you a friend request!</div>}
-        {type === 2 && <div>{title} started following you!</div>}
-        {type === 3 && <div>You got mail from {title}!</div>}
-        {type === 4 && <div>{title} added a new item to the garderobe.</div>}
-        {type === 5 && <div>{title} added a new look to the garderobe.</div>}
-
-        <div> createdAt : {createdAt}</div>
+        <div className="notifications__rightSide">
+          {type === 1 && <div>{title} sent you a friend request!</div>}
+          {type === 2 && <div>{title} started following you!</div>}
+          {type === 3 && <div>You got mail from {title}!</div>}
+          {type === 4 && <div>{title} added a new item to the garderobe.</div>}
+          {type === 5 && <div>{title} added a new look to the garderobe.</div>}
+          <div className="notifications__date"> {createdAt}</div>
+        </div>
       </div>
     );
   });
