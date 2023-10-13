32cb9eafc25e83dd636c8002cfbdfe7f56e8dca8
“vanStaen” <clement.vanstaen@gmail.com>
2023-10-13 16:20:38 +0200

    notificatiom deletable + swipe

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { observer } from "mobx-react";
 import { useNavigate } from "react-router-dom";
 import * as dayjs from "dayjs";
 import relativeTime from 'dayjs/plugin/relativeTime';
-import { CloseOutlined } from '@ant-design/icons';
+import { CloseOutlined, DeleteFilled } from '@ant-design/icons';
 dayjs.extend(relativeTime)
 
 import { pageStore } from "../../stores/pageStore/pageStore";
@@ -12,8 +12,14 @@ import { deleteNotification } from "./deleteNotification";
 
 import "./Notifications.less";
 
+// the required distance between touchStart and touchEnd to be detected as a swipe
+const MIN_SWIPE_DISTANCE = 20;
+
 export const Notifications = observer(() => {
   const navigate = useNavigate();
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+  const throttling = useRef(false);
 
   useEffect(() => {
     postNotificationsSeen();
@@ -29,49 +35,104 @@ export const Notifications = observer(() => {
     }, 300);
   }
 
+  const closeNotificationHandlerMobile = (id) => {
+    const elementNotification = document.getElementById(`notification${id}`);
+    const elementDeleteButton = document.getElementById(`deleteButton${id}`);
+    elementNotification.style.display = "none";
+    elementDeleteButton.style.display = "none";
+    deleteNotification(id);
+  }
+
   const notificationClickHandlerRedirectToProfile = (username) => {
     navigate(`/${username}`);
   }
+
+  const showDeleteHandler = (id) => {
+    const element = document.getElementById(`notification${id}`);
+    element.style.left = "-50px";
+    console.log("show", element);
+  }
+
+  const hideDeleteHandler = (id) => {
+    const element = document.getElementById(`notification${id}`);
+    element.style.left = "0";
+    console.log("hide", element);
+  }
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const onTouchEnd = (id) => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > MIN_SWIPE_DISTANCE;
+    const isRightSwipe = distance < -MIN_SWIPE_DISTANCE;
+    if (throttling.current === false) {
+      throttling.current = true;
+      if (isRightSwipe) {
+        hideDeleteHandler(id);
+      } else if (isLeftSwipe) {
+        showDeleteHandler(id);
+      }
+      setTimeout(() => {
+        throttling.current = false;
+      }, 500);
+    }
+  };
 
   const notificationsFormated = pageStore.notifications.map((notification) => {
     const { _id, type, seen, title, createdAt, media_url } = notification;
     const notificationAge = dayjs(createdAt).fromNow();
 
     return (
-      <div
-        className={`notifications__notification ${seen ? "seen" : "new"}`}
-        id={`notification${_id}`}
-      >
-        <div
-          className="notifications__leftSide"
-          onClick={() => notificationClickHandlerRedirectToProfile(title)}
-          style={{
-            background: `url(${media_url})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
-        <div
-          className="notifications__rightSide"
-          onClick={() => notificationClickHandlerRedirectToProfile(title)}
-        >
-          <div className="notification__title">
-            {type === 1 && `${title} sent you a friend request!`}
-            {type === 2 && `${title} started following you!`}
-            {type === 3 && `You got mail from ${title}!`}
-            {type === 4 && `${title} added a new item to the garderobe`}
-            {type === 5 && `${title} added a new look to the garderobe`}
+      <>
+        <div className="notifications__deleteButton" id={`deleteButton${_id}`}>
+          <div className="icon" onClick={() => closeNotificationHandlerMobile(_id)}>
+            <DeleteFilled />
           </div>
-          <div className="notifications__date"> {notificationAge}</div>
         </div>
         <div
-          className="closeDelete"
-          onClick={() => closeNotificationHandler(_id)}
+          className={`notifications__notification ${seen ? "seen" : "new"}`}
+          id={`notification${_id}`}
+          onTouchStart={onTouchStart}
+          onTouchMove={onTouchMove}
+          onTouchEnd={() => onTouchEnd(_id)}
         >
-          <CloseOutlined />
+          <div
+            className="notifications__leftSide"
+            onClick={() => notificationClickHandlerRedirectToProfile(title)}
+            style={{
+              background: `url(${media_url})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <div
+            className="notifications__rightSide"
+            onClick={() => notificationClickHandlerRedirectToProfile(title)}
+          >
+            <div className="notification__title">
+              {type === 1 && `${title} sent you a friend request!`}
+              {type === 2 && `${title} started following you!`}
+              {type === 3 && `You got mail from ${title}!`}
+              {type === 4 && `${title} added a new item to the garderobe`}
+              {type === 5 && `${title} added a new look to the garderobe`}
+            </div>
+            <div className="notifications__date"> {notificationAge}</div>
+          </div>
+          <div
+            className="closeDelete"
+            onClick={() => closeNotificationHandler(_id)}
+          >
+            <CloseOutlined />
+          </div>
         </div>
-      </div>
+      </>
     );
   });
 
