8389bb91f68c8e63a9f0e0243822fe1a2d2c8f50
vanStaen <clement.vanstaen@gmail.com>
2023-10-18 21:38:34 +0200

    fix a lot of stuff in notifications links

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { observer } from "mobx-react";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import {
   CameraOutlined,
   CloseOutlined,
@@ -13,17 +12,19 @@ import {
   SkinOutlined,
   MailOutlined,
   LinkOutlined,
-} from '@ant-design/icons';
+} from "@ant-design/icons";
 import * as dayjs from "dayjs";
-import relativeTime from 'dayjs/plugin/relativeTime';
+import relativeTime from "dayjs/plugin/relativeTime";
 
 import { pageStore } from "../../stores/pageStore/pageStore";
+import { itemsStore } from "../Items/itemsStore";
+import { looksStore } from "../Looks/looksStore";
 import { postNotificationsSeen } from "./postNotificationsSeen";
 import { deleteNotification } from "./deleteNotification";
 
 import "./Notifications.less";
 
-dayjs.extend(relativeTime)
+dayjs.extend(relativeTime);
 
 // the required distance between touchStart and touchEnd to be detected as a swipe
 const MIN_SWIPE_DISTANCE = 20;
@@ -46,7 +47,7 @@ export const Notifications = observer(() => {
       element.style.display = "none";
       deleteNotification(id);
     }, 300);
-  }
+  };
 
   const closeNotificationHandlerMobile = (id) => {
     const elementNotification = document.getElementById(`notification${id}`);
@@ -54,33 +55,72 @@ export const Notifications = observer(() => {
     elementNotification.style.display = "none";
     elementDeleteButton.style.display = "none";
     deleteNotification(id);
-  }
+  };
 
-  const notificationClickHandler = (type, title, action_data) => {
-    if (type === 1) { navigate(`/${title}`) } //Friend request
-    else if (type === 2) { navigate(`/${title}`) } //New Follower
-    else if (type === 14) { navigate(`/${title}`) } //Friend new avatar
-    else if (type === 3) { /*TODO*/ } //New Mail
-    else if (type === 4) { /*TODO*/ } //friends new item
-    else if (type === 5) { /*TODO*/ } //friends new Look
-    else if (type === 12) { navigate(`/item/${action_data}`) } //Item liked
-    else if (type === 15) { navigate(`/item/${action_data}`) } //Item disliked
-    else if (type === 13) { navigate(`/look/${action_data}`) } //Look liked
-    else if (type === 16) { navigate(`/look/${action_data}`) } //Look disliked
-  }
+  const notificationClickHandler = async (type, title, action_data) => {
+    console.log("click");
+    //Friend request
+    if (type === 1) {
+      navigate(`/${title}`);
+    }
+    //New Follower
+    else if (type === 2) {
+      navigate(`/${title}`);
+    }
+    //Friend new avatar
+    else if (type === 14) {
+      navigate(`/${title}`);
+    }
+    //New Mail
+    else if (type === 3) {
+      /*TODO*/
+    }
+    //friends new item
+    else if (type === 4) {
+      /*TODO*/
+    }
+    //friends new Look
+    else if (type === 5) {
+      /*TODO*/
+    }
+    //Item liked/disliked
+    else if (type === 12 || type === 15) {
+      if (itemsStore.isLoading === true) {
+        await itemsStore.loadItems();
+      }
+      const selectedItem = itemsStore.items.filter(
+        (item) => parseInt(item._id) === action_data
+      );
+      itemsStore.setSelectedItem(selectedItem[0]);
+      navigate(`/items/`);
+    }
+    //Look liked || disliked
+    else if (type === 13 || type === 16) {
+      if (looksStore.isLoading === true) {
+        await looksStore.loadLooks();
+      }
+      const selectedLook = looksStore.looks.filter(
+        (look) => parseInt(look._id) === action_data
+      );
+      looksStore.setSelectedLook(selectedLook[0]);
+      navigate(`/looks/`);
+    }
+  };
 
   const showDeleteHandler = (id) => {
     const elementNotification = document.getElementById(`notification${id}`);
-    const elementDeleteButtonIcon = document.getElementById(`deleteButtonIcon${id}`);
+    const elementDeleteButtonIcon = document.getElementById(
+      `deleteButtonIcon${id}`
+    );
     const newHeight = `${elementNotification.offsetHeight}px`;
     elementDeleteButtonIcon.style.height = newHeight;
     elementNotification.style.left = "-50px";
-  }
+  };
 
   const hideDeleteHandler = (id) => {
     const element = document.getElementById(`notification${id}`);
     element.style.left = "0";
-  }
+  };
 
   const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
 
@@ -108,10 +148,15 @@ export const Notifications = observer(() => {
   };
 
   const notificationsFormated = pageStore.notifications.map((notification) => {
-    const { _id, type, seen, title, createdAt, media_url, action_data } = notification;
+    const { _id, type, seen, title, createdAt, media_url, action_data } =
+      notification;
     const notificationAge = dayjs(createdAt).fromNow();
 
-    const linkToUserPage = <Link to={`/${title}`}>{title}</Link>
+    const linkToUserPage = (
+      <Link to={`/${title}`} onClick={(e) => e.stopPropagation()}>
+        {title}
+      </Link>
+    );
 
     return (
       <>
@@ -132,7 +177,9 @@ export const Notifications = observer(() => {
           onTouchEnd={() => onTouchEnd(_id)}
         >
           <div
-            className={seen ? "notifications__leftSideSeen" : "notifications__leftSide"}
+            className={
+              seen ? "notifications__leftSideSeen" : "notifications__leftSide"
+            }
             onClick={() => notificationClickHandler(type, title, action_data)}
             style={{
               background: `url(${media_url}) center center / cover no-repeat`,
@@ -146,8 +193,12 @@ export const Notifications = observer(() => {
               {type === 1 && <>{linkToUserPage} sent you a friend request!</>}
               {type === 2 && <>{linkToUserPage} started following you!</>}
               {type === 3 && <>You got mail from ${linkToUserPage}!</>}
-              {type === 4 && <>{linkToUserPage} added a new item to the garderobe</>}
-              {type === 5 && <>{linkToUserPage} added a new look to the garderobe</>}
+              {type === 4 && (
+                <>{linkToUserPage} added a new item to the garderobe</>
+              )}
+              {type === 5 && (
+                <>{linkToUserPage} added a new look to the garderobe</>
+              )}
               {type === 12 && <>{linkToUserPage} liked this Item</>}
               {type === 13 && <>{linkToUserPage} liked this Look</>}
               {type === 14 && <>{linkToUserPage} added a new profile picture</>}
@@ -181,11 +232,11 @@ export const Notifications = observer(() => {
 
   return (
     <div className="notifications__container">
-      {pageStore.notifications.length === 0
-        ? <div className="notification__nothing">
-          No notification for now
-        </div>
-        : notificationsFormated}
+      {pageStore.notifications.length === 0 ? (
+        <div className="notification__nothing">No notification for now</div>
+      ) : (
+        notificationsFormated
+      )}
     </div>
   );
 });
