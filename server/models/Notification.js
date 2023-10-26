70e6b47a97a96508c3acdd05c0a0e91f818e9c58
vanStaen <clement.vanstaen@gmail.com>
2023-10-26 17:06:08 +0200

    notifications types

--- server/models/Notification.js
+++ server/models/Notification.js
@@ -43,22 +43,22 @@ module.exports = {
 
 /*
 types:
-0: General
-1: New Friend request
-2: New Follower ✓
-3: New Message
-4: New item from friends/followed ✓
-5: New look from friend/followed ✓
-6: Someone shared an item with you (theirs)
-7: Someone wants to share an item (yours)
-8: Someone made you a Look
-9: Someone has an Item for sale
-10: Someone has an Item to give away
-11: Someone used your Item in a Look
-12: New Item like
-13: New Look like
-14: New Avatar from friends/followed ✓
-15: New Item dislike
-16: New Look dislike
-17: Friend requested accepted
+  0: General
+  1: New Friend request ✓
+  2: New Follower ✓
+  3: New Message
+  4: New item from friends/followed ✓
+  5: New look from friend/followed ✓
+  6: Someone shared an item with you (theirs)
+  7: Someone wants to share an item (yours)
+  8: Someone made you a Look
+  9: Someone has an Item for sale
+  10: Someone has an Item to give away
+  11: Someone used your Item in a Look
+  12: New Item like ✓
+  13: New Look like ✓
+  14: New Avatar from friends/followed ✓
+  15: New Item dislike ✓
+  16: New Look dislike ✓
+  17: Friend requested accepted ✓
 */
