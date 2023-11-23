0681848f0704ebfd86741da7423a5b43c42b4947
vanStaen <clement.vanstaen@gmail.com>
2023-11-23 19:26:38 +0100

    footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "./Footer.less";
 
 export const Footer = () => {
@@ -6,15 +8,21 @@ export const Footer = () => {
     <div className="footerContainer">
       <div className="footerSubContainer1">
         <div className="col">
-          <div className="title">About</div>
-          <div>Our Project</div>
-          <div>About us</div>
-        </div>
-        <div className="col">
           <div className="title">Contact</div>
           <div>Report a Bug</div>
           <div>Technical Help</div>
           <div>Write us an email</div>
+        </div>
+        <div className="col">
+          <div className="title">Links</div>
+          <Link to="/info">
+            <div className="link">The Project</div>
+          </Link>
+          <div className="link">
+            <a href="https://www.clementvanstaen.com" target="_blank">
+              clementvanstaen.com
+            </a>
+          </div>
         </div>
         <div className="col">
           <div className="title">Socials</div>
@@ -24,6 +32,7 @@ export const Footer = () => {
           <span>Get informed about all changes of Rewear App.</span>
         </div>
       </div>
+
       <div className="footerSubContainer2">
         <div className="leftSide">©{new Date().getFullYear()} REWAER</div>
         <div className="rightSide"> Impressum | Datenschutz & Cookie</div>
