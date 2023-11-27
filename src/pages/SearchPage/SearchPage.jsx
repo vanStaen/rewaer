60756e2e994e22011304a36268a11f852a896022
vanStaen <clement.vanstaen@gmail.com>
2023-11-27 22:10:48 +0100

    Search update WIP

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "antd";
 import { Link } from "react-router-dom";
 
@@ -18,6 +18,7 @@ const { Search } = Input;
 
 export const SearchPage = () => {
   const [isSearching, setIsSearching] = useState(false);
+  const [searchValue, setSearchValue] = useState(false);
   const [results, setResults] = useState(null);
 
   const handleEnter = async (e) => {
@@ -27,6 +28,7 @@ export const SearchPage = () => {
       return;
     }
     setIsSearching(true);
+    setSearchValue(e.target.value);
     const response = await postSearchMore(e.target.value);
     if (response) {
       setResults(response);
@@ -43,6 +45,7 @@ export const SearchPage = () => {
       return;
     }
     setIsSearching(true);
+    setSearchValue(e.target.value);
     const response = await postSearch(e.target.value);
     if (response) {
       setResults(response);
@@ -59,6 +62,7 @@ export const SearchPage = () => {
       return;
     }
     setIsSearching(true);
+    setSearchValue(value);
     const response = await postSearchMore(value);
     if (response) {
       setResults(response);
@@ -67,6 +71,25 @@ export const SearchPage = () => {
     }
     setIsSearching(false);
   };
+
+  useEffect(() => {
+    if (results) {
+      const resultElement = document.getElementById("resultContent");
+      console.log("Result element", resultElement);
+      console.log("searchValue", searchValue);
+      if (searchValue !== "") {
+        const content = resultElement.textContent;
+        const regex = new RegExp(searchValue, "gi");
+        const highlightedContent = content.replace(
+          regex,
+          (match) => `<span class="highlight">${match}</span>`
+        );
+        //resultElement.innerHTML = highlightedContent;
+      } else {
+        //resultElement.innerHTML = resultElement.textContent;
+      }
+    }
+  }, [results]);
 
   return (
     <div className="search__container">
@@ -84,114 +107,117 @@ export const SearchPage = () => {
         <div className="search__results">Results: {results.count}</div>
       )}
 
-      {results && results.users.length > 0 && (
-        <div className="search__subContainer">
-          <div className="search__title">Users</div>
-          {results.users.map((user) => {
-            return (
-              <Link
-                to={`/${user.userName}`}
-                onClick={() => {
-                  profileStore.fetchProfileData(user.userName);
-                }}
-              >
+      <div id="resultContent">
+        {results && results.users.length > 0 && (
+          <div className="search__subContainer">
+            <div className="search__title">Users</div>
+            {results.users.map((user) => {
+              return (
+                <Link
+                  to={`/${user.userName}`}
+                  onClick={() => {
+                    profileStore.fetchProfileData(user.userName);
+                  }}
+                >
+                  <div className="search__resultItem">
+                    <div className="search__resultItemPictures">
+                      <img src={user.avatar} className="search__picture"></img>
+                    </div>
+                    <div className="search__resultItemData">
+                      <div className="search__resultItemDataRow bold">
+                        {user.userName}
+                      </div>
+                      <div className="search__resultItemDataRow grey">
+                        {user.firstName} {user.lastName}
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+        )}
+        {results && results.items.length > 0 && (
+          <div className="search__subContainer">
+            <div className="search__title">Items</div>
+            {results.items.map((item) => {
+              return (
                 <div className="search__resultItem">
                   <div className="search__resultItemPictures">
-                    <img src={user.avatar} className="search__picture"></img>
+                    <img
+                      src={item.mediaUrlThumb}
+                      className="search__picture"
+                    ></img>
                   </div>
                   <div className="search__resultItemData">
                     <div className="search__resultItemDataRow bold">
-                      {user.userName}
+                      {item.title}
                     </div>
                     <div className="search__resultItemDataRow grey">
-                      {user.firstName} {user.lastName}
+                      {item.brand && `${item.brand}  `}
+                      {item.colors.length > 0 &&
+                        item.colors.map((color) => {
+                          return `${
+                            convertCodeToObjectString(color, colors)[
+                              userStore.language
+                            ]
+                          }  `;
+                        })}
+                      {item.pattern &&
+                        `${
+                          convertCodeToObjectString(item.pattern, pattern)[
+                            userStore.language
+                          ]
+                        }  `}
                     </div>
                   </div>
                 </div>
-              </Link>
-            );
-          })}
-        </div>
-      )}
-      {results && results.items.length > 0 && (
-        <div className="search__subContainer">
-          <div className="search__title">Items</div>
-          {results.items.map((item) => {
-            return (
-              <div className="search__resultItem">
-                <div className="search__resultItemPictures">
-                  <img
-                    src={item.mediaUrlThumb}
-                    className="search__picture"
-                  ></img>
-                </div>
-                <div className="search__resultItemData">
-                  <div className="search__resultItemDataRow bold">
-                    {item.title}
+              );
+            })}
+          </div>
+        )}
+        {results && results.looks.length > 0 && (
+          <div className="search__subContainer">
+            <div className="search__title">Looks</div>
+            {results.looks.map((look) => {
+              return (
+                <div className="search__resultItem">
+                  <div className="search__resultItemPictures">
+                    <img
+                      src={look.mediaUrlThumb}
+                      className="search__picture"
+                    ></img>
                   </div>
-                  <div className="search__resultItemDataRow grey">
-                    {item.brand && `${item.brand}  `}
-                    {item.colors.length > 0 &&
-                      item.colors.map((color) => {
-                        return `${
-                          convertCodeToObjectString(color, colors)[
+                  <div className="search__resultItemData">
+                    <div className="search__resultItemDataRow bold">
+                      {look.title}
+                    </div>
+                    <div className="search__resultItemDataRow grey">
+                      {!!look.category &&
+                        `${
+                          convertCodeToObjectString(
+                            look.category,
+                            lookCategory
+                          )[userStore.language]
+                        }  `}
+                      {!!look.season &&
+                        `${
+                          convertCodeToObjectString(look.season, seasons)[
                             userStore.language
                           ]
-                        }  `;
-                      })}
-                    {item.pattern &&
-                      `${
-                        convertCodeToObjectString(item.pattern, pattern)[
-                          userStore.language
-                        ]
-                      }  `}
+                        }  `}
+                      {look.items.length > 0 &&
+                        `${look.items.length} item${
+                          look.items.length > 1 ? "s" : ""
+                        }  `}
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-      )}
-      {results && results.looks.length > 0 && (
-        <div className="search__subContainer">
-          <div className="search__title">Looks</div>
-          {results.looks.map((look) => {
-            return (
-              <div className="search__resultItem">
-                <div className="search__resultItemPictures">
-                  <img
-                    src={look.mediaUrlThumb}
-                    className="search__picture"
-                  ></img>
-                </div>
-                <div className="search__resultItemData">
-                  <div className="search__resultItemDataRow bold">
-                    {look.title}
-                  </div>
-                  <div className="search__resultItemDataRow grey">
-                    {!!look.category &&
-                      `${
-                        convertCodeToObjectString(look.category, lookCategory)[
-                          userStore.language
-                        ]
-                      }  `}
-                    {!!look.season &&
-                      `${
-                        convertCodeToObjectString(look.season, seasons)[
-                          userStore.language
-                        ]
-                      }  `}
-                    {look.items.length > 0 &&
-                      `${look.items.length} item${
-                        look.items.length > 1 ? "s" : ""
-                      }  `}
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      )}
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/SearchPage/postSearch.js
+++ src/pages/SearchPage/postSearch.js
@@ -20,6 +20,5 @@ export const postSearch = async (searchText) => {
         }
     }
 
-    console.log("search result", response.data)
     return response.data;
 };
