58e241db4dbc7daace3aeff22468007dd799bf28
vanStaen <clement.vanstaen@gmail.com>
2025-07-23 14:45:35 +0200

    mail page palceholder as tsx

--- src/pages/Mail/Mail.tsx
+++ src/pages/Mail/Mail.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-class MailPage extends Component {
+class MailPage extends Component<Record<string, unknown>> {
   render() {
     return <h1>Messenger</h1>;
   }
