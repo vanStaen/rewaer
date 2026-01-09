a2066b0aec55bc8874600013e14f4582495c53ef
vanStaen <clement.vanstaen@gmail.com>
2026-01-09 19:52:56 +0100

    run linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useCallback } from "react";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { notification } from "antd";
 import { observer } from "mobx-react";
 import { useTranslation } from "react-i18next";
