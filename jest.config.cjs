2794a01ef3dac4e76a2cb198c6376ac1311138af
vanStaen <clement.vanstaen@gmail.com>
2026-01-09 23:42:03 +0100

    fix test

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
   moduleNameMapper: {
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/mocks/fileMock.js",
